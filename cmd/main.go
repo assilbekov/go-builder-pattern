425798277cc5c82f2e62f2af13ee8a5f85f3cabc
assilbekov <askhat.assilbekov@gmail.com>
2024-04-28 18:44:50 -0300

    implement computers creation

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "builder/pkg"
+
 func main() {
-	//
+	asusCollector := pkg.GetCollector(pkg.AsusCollectorType)
+	msiCollector := pkg.GetCollector(pkg.MsiCollectorType)
+	factory := pkg.NewFactory(asusCollector)
+	asusComputer := factory.CreateComputer()
+	asusComputer.Print()
+	factory.SetCollector(msiCollector)
+	msiComputer := factory.CreateComputer()
+	msiComputer.Print()
 }
