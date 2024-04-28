9a2a2c0773c539954080c53ff1077ad2fae2a914
assilbekov <askhat.assilbekov@gmail.com>
2024-04-28 18:28:50 -0300

    add print method for computer

--- pkg/computer.go
+++ pkg/computer.go
@@ -1,8 +1,17 @@
 package pkg
 
+import "fmt"
+
 type Computer struct {
 	Core        int
 	Brand       string
 	Memory      int
+	Monitor     int
 	GraphicCard int
+}
+
+func (c *Computer) Print() {
+	fmt.Println(
+		"%s Core: [%d] Memory: [%d] GraphicCard: [%d] Monitor: [%d]\n",
+		c.Brand, c.Core, c.Memory, c.GraphicCard, c.Monitor)
 }
